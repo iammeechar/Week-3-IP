e33dc9bff7d7e19863d84dd1979f4edcfa089959
iammeechar <aldomicha@gmail.com>
2021-04-20 11:18:16 +0300

    add modify form functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,55 @@
-$(document).ready(function(){
-    $("#des-image").click(function(){
-        $("#des-image").toggle();
-        $("#design").toggle();
+$(document).ready(function () {
+    $(".img1").click(function () {
+      $(".img1").toggle(function () {
+        $(".para1", ).show(function () {
+          $("#h3a").css('font-weight', 'bold');
+        });
+      });
     });
-});
+  
+    $(".img2").click(function () {
+      $(".img2").toggle(function () {
+        $(".para2").show(function () {
+          $("#h3b").css('font-weight', 'bold');
+        });
+      });
+    });
+  
+    $(".img3").click(function () {
+      $(".img3").toggle(function () {
+        $(".para3").show(function () {
+          $("#h3c").css('font-weight', 'bold');
+        });
+      });
+    });
+  
+    $(".para1").click(function () {
+      $(".para1").toggle(function () {
+        $(".img1").show(function () {
+          $("#h3a").css('font-weight', 'normal');
+        });
+      });
+    });
+  
+    $(".para2").click(function () {
+      $(".para2").toggle(function () {
+        $(".img2").show(function () {
+          $("#h3b").css('font-weight', 'normal');
+        });
+      });
+    });
+  
+    $(".para3").click(function () {
+      $(".para3").toggle(function () {
+        $(".img3").show(function () {
+          $("#h3c").css('font-weight', 'normal');
+        });
+      });
+    });
+    $("#contact").submit(function (e) {
 
-$(document).ready(function(){
-    $("#contacts form".submit(function(event){
-       var contacts = ["name", "email", "message"];
-    blanks.forEach(function(blank){
-        var userInput = $("input#" + contacts).val();
-        $("." + contacts).text(userInput);
-    });
-        event.preventDefault();
-    }));
-});
+        var user = document.getElementById("name").value;
+        alert(user + " your message has been recieved successfully and thanks for keeping in touch");
+    
+      });
+});    
